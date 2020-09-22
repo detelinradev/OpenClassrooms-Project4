064bf3f53fbfcc9c335f321833d5cb378020c6de
Detelin Radev <clover88bg@yahoo.com>
2020-09-22 16:56:41 +0300

    Moved the logic for calculating parking fare to enum DiscountType to get in line with Open/Close SOLID principle

--- src/main/java/com/parkit/parkingsystem/constants/DiscountType.java
+++ src/main/java/com/parkit/parkingsystem/constants/DiscountType.java
@@ -1,5 +1,25 @@
 package com.parkit.parkingsystem.constants;
 
 public enum DiscountType {
-    NO_DISCOUNT, FREE_30_MIN, RECURRING_USERS_5PERCENT
+    NO_DISCOUNT{
+        @Override
+        public double calculatePrice(Double price, long duration, double vehicleRatePerHour) {
+
+            return price;
+        }
+    }, FREE_30_MIN {
+        @Override
+        public double calculatePrice(Double price, long duration, double vehicleRatePerHour) {
+
+            return price;
+        }
+    }, RECURRING_USERS_5PERCENT {
+        @Override
+        public double calculatePrice(Double price, long duration, double vehicleRatePerHour) {
+
+            return price;
+        }
+    };
+
+    public abstract double calculatePrice(Double price, long duration, double vehicleRatePerHour);
 }
--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorServiceImpl.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorServiceImpl.java
@@ -5,24 +5,29 @@ import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.contracts.FareCalculatorService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FareCalculatorServiceImpl implements FareCalculatorService {
 
-    private final DiscountType discountType;
+    private static final List<DiscountType> discounts = new ArrayList<>();
 
     private FareCalculatorServiceImpl(Builder builder) {
-        this.discountType = builder.discountType;
+        DiscountType discountType = builder.discountType;
     }
 
     public static class Builder {
 
         private DiscountType discountType;
 
-        public Builder(DiscountType discountType){
+        public Builder(DiscountType discountType) {
             this.discountType = discountType;
+            discounts.add(discountType);
         }
 
         public Builder withDiscountType(DiscountType discountType) {
             this.discountType = discountType;
+            discounts.add(discountType);
             return this;
         }
 
@@ -39,18 +44,34 @@ public class FareCalculatorServiceImpl implements FareCalculatorService {
         long inMinute = ticket.getInTime();
         long outMinute = ticket.getOutTime();
         long duration = (outMinute - inMinute) / 60;
+        double price = 0.0;
 
         switch (ticket.getParkingSpot().getParkingType()) {
+
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR / 60);
+
+                for(DiscountType discount : discounts){
+                   price = discount.calculatePrice(price, duration, Fare.CAR_RATE_PER_HOUR);
+                }
+                ticket.setPrice(price);
+
                 break;
             }
+
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR / 60);
+
+                for(DiscountType discount : discounts){
+                    price = discount.calculatePrice(price, duration, Fare.BIKE_RATE_PER_HOUR);
+                }
+                ticket.setPrice(price);
+
                 break;
             }
+
             default:
                 throw new IllegalArgumentException("Unknown Parking Type");
         }
+
+//        ticket.setPrice(price);
     }
 }
