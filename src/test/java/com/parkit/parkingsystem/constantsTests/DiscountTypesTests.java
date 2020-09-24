a2d230bd7160f856c42ecaaa9acd8536a6e64811
Detelin Radev <clover88bg@yahoo.com>
2020-09-24 09:22:38 +0300

    Added unit and integration tests for feature 5 percent discount for recurring users

--- src/test/java/com/parkit/parkingsystem/constantsTests/DiscountTypesTests.java
+++ src/test/java/com/parkit/parkingsystem/constantsTests/DiscountTypesTests.java
@@ -65,4 +65,65 @@ public class DiscountTypesTests {
         //assert
         Assertions.assertEquals(35.25,price);
     }
+
+    @Test
+    public void calculate_Price_Should_ReturnCorrectPrice_When_DiscountTypeIsRECURRING_USERS_5PERCENTAndNODISCOUNTAndDurationIsLessThenHour(){
+
+        //act
+        double price = DiscountType.RECURRING_USERS_5PERCENT.calculatePrice(1.125,45,1.5,true);
+
+        //assert
+        Assertions.assertEquals(1.0687499999999999,price);
+    }
+
+    @Test
+    public void calculate_Price_Should_ReturnCorrectPrice_When_DiscountTypeIsRECURRING_USERS_5PERCENTAndNODISCOUNTAndDurationIsMoreThenHour(){
+
+        //act
+        double price = DiscountType.RECURRING_USERS_5PERCENT.calculatePrice(3.0,120,1.5,true);
+
+        //assert
+        Assertions.assertEquals(2.8499999999999996,price);
+    }
+
+    @Test
+    public void calculate_Price_Should_ReturnCorrectPrice_When_DiscountTypeIsRECURRING_USERS_5PERCENTAndNODISCOUNTAndDurationIsMoreThenADay(){
+
+        //act
+        double price = DiscountType.RECURRING_USERS_5PERCENT.calculatePrice(36,1440,1.5,true);
+
+        //assert
+        Assertions.assertEquals(34.199999999999996,price);
+    }
+
+    @Test
+    public void calculate_Price_Should_ReturnCorrectPrice_When_DiscountTypeIsRECURRING_USERS_5PERCENTAndFREE30MINAndDurationIsLessThen30Min(){
+
+        //act
+        double price = DiscountType.RECURRING_USERS_5PERCENT.calculatePrice(0.0,45,1.5,true);
+
+        //assert
+        Assertions.assertEquals(0.0,price);
+    }
+
+    @Test
+    public void calculate_Price_Should_ReturnCorrectPrice_When_DiscountTypeIsRECURRING_USERS_5PERCENTAndFREE30MINAndDurationIsMoreThenHour(){
+
+        //act
+        double price = DiscountType.RECURRING_USERS_5PERCENT.calculatePrice(2.25,120,1.5,true);
+
+        //assert
+        Assertions.assertEquals(2.1374999999999997,price);
+    }
+
+    @Test
+    public void calculate_Price_Should_ReturnCorrectPrice_When_DiscountTypeIsRECURRING_USERS_5PERCENTAndFREE30MINAndDurationIsMoreThenADay(){
+
+        //act
+        double price = DiscountType.RECURRING_USERS_5PERCENT.calculatePrice(35.25,1440,1.5,true);
+
+        //assert
+        Assertions.assertEquals(33.4875,price);
+    }
 }
+
