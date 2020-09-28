ec838ea6b7db732f5dbf843ba7bb20a04e3a5f34
Detelin Radev <clover88bg@yahoo.com>
2020-09-28 13:15:55 +0300

    added javadocs to exception package

--- src/main/java/com/parkit/parkingsystem/exception/UnsuccessfulOperationException.java
+++ src/main/java/com/parkit/parkingsystem/exception/UnsuccessfulOperationException.java
@@ -1,12 +1,32 @@
 package com.parkit.parkingsystem.exception;
 
+/**
+ * Creates more business specific exception able to present otherwise not
+ * exceptional states as exceptional following the application logic.
+ */
 public class UnsuccessfulOperationException extends RuntimeException {
 
+    /**
+     * Constructs a new exception with the specified detail message.
+     *
+     * @param errorMessage  <code>String</code> variable as parameter
+     *                    representing error message explaining the cause for
+     *                    the exception
+     */
     public UnsuccessfulOperationException(String errorMessage) {
 
         super(errorMessage);
     }
 
+    /**
+     * Constructs a new exception with the specified detail message and causing
+     * exception.
+     *
+     * @param errorMessage  <code>String</code> variable as parameter
+     *                     representing error message explaining the cause for
+     *                     the exception
+     * @param error  instance of <code>Throwable</code> causing this exception
+     */
     public UnsuccessfulOperationException(String errorMessage, Throwable error) {
 
         super(errorMessage, error);
