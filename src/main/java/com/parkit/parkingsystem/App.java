87b10c3e6e34db87dd1ddb838863d936f82b0d06
Detelin Radev <clover88bg@yahoo.com>
2020-09-28 19:56:02 +0300

    added javadocs to app class, although it is considered non sense, in that case the mechanism of calculating discounts needs to be clearly explained where it is to be used, so the fare calculating logic works promptly

--- src/main/java/com/parkit/parkingsystem/App.java
+++ src/main/java/com/parkit/parkingsystem/App.java
@@ -7,8 +7,36 @@ import com.parkit.parkingsystem.service.InteractiveShellImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+/**
+ *  Encapsulates the application's main entry point.
+ *
+ * @author Detelin Radev
+ */
 public class App {
+
     private static final Logger logger = LogManager.getLogger("App");
+
+    /**
+     * The application's entry point.
+     * <p>
+     * Instantiates <code>InteractiveShell</code>, which have
+     * <code>FareCalculatorServiceImpl</code> instance as parameter in its
+     * constructor. <code>FareCalculatorServiceImpl</code> itself relies
+     * on Builder to construct its instance, and the Builder takes as
+     * parameter <code>DiscountType</code> <code>enum</code>.
+     *
+     * It is mandatory that this parameter is primary <code>DiscountType</code>,
+     * means it starts with P_ due to the way of calculating parking fare and if
+     * there is more attached <code>DiscountType</code> they should be of
+     * supplementary type, means start with S_.
+     *
+     * A chain of discounts should only have one primary discount and it should
+     * be passed as parameter to the Builder. All other discounts should be
+     * supplementary and added through the Builder's method
+     * <code>withDiscountType</code>
+     *
+     * @param args an array of command-line arguments for the application
+     */
     public static void main(String[] args){
         logger.info("Initializing Parking System");
         InteractiveShell interactiveShell = new InteractiveShellImpl(new FareCalculatorServiceImpl
